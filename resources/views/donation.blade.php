e9256773fa5df23114f7d6189622edcf1c90b581
Alfiannsptra <alfiansaputra355@gmail.com>
2024-01-23 11:26:10 +0800

    Halaman Donation

--- resources/views/donation.blade.php
+++ resources/views/donation.blade.php
@@ -42,21 +42,32 @@
 
     <main id="main">
         <div class="container login-container" style="margin-top: 7rem; margin-bottom: 3rem">
-            <div class="row justify-content-center">
-                <div class="col-md-5">
-                    <div class="card p-5 rounded-3">
+            <div class="text-center">
+                <h1 class="fw-bold" style="color: #38527E">Dataset Donation Form</h1>
+                <h5 style="color: gray">We offer users the option to upload their dataset data to our repository.</h5>
+                <h5 style="color: gray">Users can provide tabular or non-tabular dataset data which will be made publicly
+                    available on our
+                    repository. Donators are free to edit their donated datasets, but edits must be approved before
+                    finalizing.</h5>
+            </div>
+
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-8">
+                    <h6 class="card-title fs-2 text-start" style="color: #38527E; margin-bottom: 1rem;">Basic
+                        Info</h6>
+                    <div class="card p-4 rounded-3">
                         <div class="card-body ">
                             <form>
-                                <h4 class="fs-2" style="color: #38527E">Forgot Password</h4>
                                 <div class="mb-3">
-                                    <label for="email" class="form-label">Email</label>
+                                    <label for="email" class="form-label">Dataset Namee </label>
                                     <input type="email" class="form-control" id="email" placeholder="">
+                                </div>
 
-                                    <button type="submit" class="btn  w-100 mt-4" style="background-color: #38527E">
-                                        <h5 class="text-light mt-2">Submit</h5>
-                                    </button>
-
+                                <!-- Button ditempatkan di luar tag div dengan class mb-3 -->
                             </form>
+                            <button type="submit" class="btn w-100 mt-4" style="background-color: #38527E">
+                                <h5 class="text-light mt-2">Submit</h5>
+                            </button>
                         </div>
                     </div>
                 </div>
