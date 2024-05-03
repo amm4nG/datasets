85f86338ae89bfabe5d82d906019cdddd31c6d46
ammangdeveloper <ammangdevelope@gmail.com>
2024-05-04 06:27:10 +0800

    fix

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,9 +5,9 @@
         <div class="container p-3" style="margin-top: 9rem">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="fw-bold mb-4" style="color: #38527E"><a style="color: #38527E" href="{{ url('datasets') }}">
+                    <h2 class="mb-5" style="color: #38527E"><a style="color: #38527E" href="{{ url('datasets') }}">
                         <i class="far fa-angle-double-left fs-3 me-2"></i></a>Detail Dataset</h2>
-                    <div class="card p-3">
+                    <div class="card p-5">
                         <div class="row align-items-center">
                             <div class="col-md-1" id="img-dataset">
                                 <i class="fad fa-database fa-4x" style="color: #38527E"></i>
@@ -19,12 +19,12 @@
                                 <p><span class="">Creator by {{ $dataset->full_name }}</span></p>
 
                             </div>
-                            <div class="col-md-12 ms-3">
+                            <div class="col-md-12">
                                 <a href="{{ url('download/' . $id) }}" class="btn btn-sm mb-3 text-white p-2"
                                     style="background-color: #38527E"><i class="bi bi-download me-1"></i> Download</a>
                                 <p>{{ $dataset->abstract }}</p>
                             </div>
-                            <div class="col-md-3 ms-3">
+                            <div class="col-md-3">
                                 <h4>Dataset Characteristics</h4>
                                 <p>
                                     @foreach ($characteristics as $characteristic)
@@ -32,13 +32,13 @@
                                     @endforeach
                                 </p>
                             </div>
-                            <div class="col-md-3 ms-3">
+                            <div class="col-md-3">
                                 <h4>Subject Area</h4>
                                 <p>
                                     {{ $dataset->name_subject_area }}
                                 </p>
                             </div>
-                            <div class="col-md-3 ms-3">
+                            <div class="col-md-3">
                                 <h4>Associated Tasks</h4>
                                 <p>
                                     @foreach ($associatedTasks as $associatedTask)
@@ -46,7 +46,7 @@
                                     @endforeach
                                 </p>
                             </div>
-                            <div class="col-md-3 ms-3">
+                            <div class="col-md-3">
                                 <h4>Feature Type</h4>
                                 <p>
                                     @foreach ($featureTypes as $featureType)
@@ -54,11 +54,11 @@
                                     @endforeach
                                 </p>
                             </div>
-                            <div class="col-md-3 ms-3">
+                            <div class="col-md-3">
                                 <h4># Instances</h4>
                                 <p>{{ $dataset->instances }}</p>
                             </div>
-                            <div class="col-md-3 ms-3">
+                            <div class="col-md-3">
                                 <h4># Features</h4>
                                 <p>{{ $dataset->features }}</p>
                             </div>
--- resources/views/my-dataset.blade.php
+++ resources/views/my-dataset.blade.php
@@ -4,7 +4,7 @@
         <div class="container login-container p-3" style="margin-top: 9rem; margin-bottom: 3rem">
             <div class="row justify-content-center">
                 <div class="col-md-12">
-                    <h2 class="fw-bold mb-4" style="color: #38527E"><a style="color: #38527E" href="{{ url('donation') }}"><i class="far fa-angle-double-left me-2 fs-3"></i></a>My Datasets</h2>
+                    <h2 class="mb-5" style="color: #38527E"><a style="color: #38527E" href="{{ url('donation') }}"><i class="far fa-angle-double-left me-2 fs-3"></i></a>My Datasets</h2>
                     <div class="card p-4">
                         <div class="table-responsive">
                             <table id="my-datasets" class="text-center table table-bordered table-striped table-sm mt-3">
